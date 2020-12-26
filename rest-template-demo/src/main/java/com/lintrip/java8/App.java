4dbe1879fa8039fcf1324f755475e9ef0264bb6c
飞翔的花狸猫 <gaoyt2012@foxmail.com>
2020-12-26 14:29:39 +0800

    RestTemplate 示例

---
+++ rest-template-demo/src/main/java/com/lintrip/java8/App.java
@@ -0,0 +1,22 @@
+package com.lintrip.java8;
+
+import org.apache.http.client.config.RequestConfig;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        RequestConfig requestConfig = RequestConfig.custom().setConnectionRequestTimeout(1000)
+                .setConnectTimeout(2000).build();
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setRequestFactory(factory);
+        ResponseEntity<String> response = restTemplate.getForEntity("http://www.baidu.com", String.class);
+        System.out.println(response.getBody());
+    }
+}
